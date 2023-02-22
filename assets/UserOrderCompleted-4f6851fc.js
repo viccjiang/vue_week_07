6037b742ef7eccc33c3df0f4f40629a74f50c3cf
jiangvicc <binbinbb520@gmail.com>
2023-02-22 11:37:03 +0800

    deploy

--- assets/UserOrderCompleted-4f6851fc.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as r,c}from"./index-9ab703ab.js";const o={};function t(n,s){return r(),c("div")}const a=e(o,[["render",t]]);export{a as default};
---
+++ assets/UserOrderPayment-ad2e1abf.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,c}from"./index-fcf79f1c.js";const t={};function n(o,s){return r(),c("div")}const _=e(t,[["render",n]]);export{_ as default};
