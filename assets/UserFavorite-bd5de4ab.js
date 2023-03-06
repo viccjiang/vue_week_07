4904e521d18c8a0363c756023734fa466521ae5a
jiangvicc <binbinbb520@gmail.com>
2023-03-06 12:48:05 +0800

    deploy

--- assets/UserFavorite-bd5de4ab.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as r,b as c}from"./index-3db46a33.js";const o={};function t(n,s){return r(),c("div",null," 我的收藏 ")}const _=e(o,[["render",t]]);export{_ as default};
---
+++ assets/UserFavorite-cb7d5f92.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,b as c}from"./index-764b8113.js";const o={};function t(n,s){return r(),c("div",null," 我的收藏 ")}const _=e(o,[["render",t]]);export{_ as default};
